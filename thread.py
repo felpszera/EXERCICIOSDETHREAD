e9bb3a2436085946aaf66acca14411aa12bedc96
Felipe Lima <63978383+felpszera@users.noreply.github.com>
2025-08-29 20:41:14 -0300

    Update thread.py
    adicionando comentários

--- thread.py
+++ thread.py
@@ -8,21 +8,22 @@ Comente o máximo possível no código, explicando os detalhes de cada ponto.
 Entregar um arquivo em formato .py por meio de repositório GitHub.
 """
 
-import threading
-import time
+import threading #importa biblioteca
+import time #importa biblioteca
 
-def x (nome, inicio, fim):
-    for i in range (inicio, fim +1):
-        print(f"{nome} -> {i}")
-        time.sleep(0.5)
+def x (nome, inicio, fim): #cria uma função X
+    for i in range (inicio, fim +1): #cria um laço de repetição para incrementar de 1 em 1
+        print(f"{nome} -> {i}") #imprime o nome e o incremento a cada repetição
+        time.sleep(0.5) #o tempo de uma repetição para outra é de 0.5 segundos
 
-thread1 = threading.Thread(target=x, args=("Thread1", 1, 10))
-thread2 = threading.Thread(target=x, args=("Thread2", 20, 30))
+thread1 = threading.Thread(target=x, args=("Thread1", 1, 10)) #cria a thread1 e relaciona o laço de repetição a ela, e ainda informa o inicio e o fim da contagem.
+thread2 = threading.Thread(target=x, args=("Thread2", 20, 30)) #cria a thread2 e relaciona o laço de repetição a ela, e ainda informa o inicio e o fim da contagem.
 
-thread1.start()
-thread2.start()
+thread1.start() #inicia a thread1
+thread2.start() #inicia a thread1
 
-thread1.join()
-thread2.join()
+thread1.join() #finaliza thread1
+thread2.join() #finaliza a thread2
 
-print("Programa finalizado")
+print("Programa finalizado") #imprime mensagem que finaliza o programa
+
